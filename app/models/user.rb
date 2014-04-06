16699a3f01af32a4640d634c71fd618ad5b300c2
qdeaqa <519421832@qq.com>
2014-04-06 14:02:08 +0800

    加入中文错误提示信息

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,9 +20,10 @@ class User < ActiveRecord::Base
   before_save :create_remember_token
 
   has_secure_password
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
-  validates :password, presence: true,length:{minimum: 6}
+  validates :password,length:{minimum: 6}
   validates :password_confirmation, presence: true
   
   def name
